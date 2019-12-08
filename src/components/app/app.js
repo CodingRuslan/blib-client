f716e92bf3e9921fccc7bd88af9c836ad8826900
CodingRuslan <orion.hola@gmail.com>
2019-12-08 16:21:10 +0300

    feat: add header and footer

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./app.css";
 import { Route, Switch } from "react-router-dom";
+import Footer from "../footer";
+import Header from "../header";
 import { HomePage, LoginPage } from "../pages";
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/login" component={LoginPage} />
-    </Switch>
+    <div className="main">
+      <Header />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+      <Footer />
+    </div>
   );
 };
 
---
+++ src/components/header/header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "react-router-dom";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className="menuButton"
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Typography variant="h4" className="title">
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            Plib
+          </Link>
+        </Typography>
+
+        <Typography variant="h5" className="title">
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            My Library
+          </Link>
+        </Typography>
+
+        <Typography variant="h5" className="title">
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            Something else
+          </Link>
+        </Typography>
+
+        <Link to="/login" style={{ textDecoration: "none", color: "white" }}>
+          <Button color="inherit">Login</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+}
