f23fc5d79560f5c873dd297be949362d665210ce
Ruslan <r.gvozd@invatechs.com>
2019-12-11 23:02:47 +0300

    feat: add modal

---
+++ src/components/edit-form/edit-form.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Modal from '@material-ui/core/Modal';
+import './edit-form.css'
+
+export default class EditForm extends Component{
+    constr
+
+    render() {
+        return (
+            <div>
+                <Modal
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <div className="paper">
+                        <h2 id="simple-modal-title">Text in a modal</h2>
+                        <p id="simple-modal-description">
+                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </p>
+                    </div>
+                </Modal>
+            </div>
+        );
+}
+}
--- src/components/product-list-item/product-list-item.js
+++ src/components/product-list-item/product-list-item.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -6,64 +6,66 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import EditForm from "../edit-form";
 import "./product-list-item.css";
 
-const useStyles = makeStyles(theme => ({
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8)
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column"
-  },
-  cardMedia: {
-    paddingTop: "56.25%" // 16:9
-  },
-  cardContent: {
-    flexGrow: 1
+
+export default class ProductListItem extends Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      openEditWindow: false
+    };
   }
-}));
 
-const ProductListItem = props => {
-  const classes = useStyles();
-  const { product } = props;
-  const { title, description, price } = product; // productId, libId,title,description, price,stars,parent, tag1,tag2, tag3
+  handleOpen = () => {
+    this.setState({
+      ...this.state,
+      openEditWindow: true
+    })
+  };
 
-  return (
-    <Grid item xs={12} sm={6} md={4}>
-      <Card className={classes.card}>
-        <CardMedia
-          className={classes.cardMedia}
-          image="https://source.unsplash.com/random"
-          title="Image title"
-        />
-        <CardContent className={classes.cardContent}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {title}
-          </Typography>
-          <Typography>
-            <i>Description: </i>
-            {description}
-          </Typography>
-          <Typography>
-            <i>Price: </i>
-            {price}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            View
-          </Button>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
-  );
+  render() {
+    const { product } = this.props;
+    const { title, description, price } = product; // productId, libId,title,description, price,stars,parent, tag1,tag2, tag3
+
+    return (
+        <>
+      <EditForm/>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card className="card">
+          <CardMedia
+            className="cardMedia"
+            image="https://source.unsplash.com/random"
+            title="Image title"
+          />
+          <CardContent className="cardContent">
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography>
+              <i>Description: </i>
+              {description}
+            </Typography>
+            <Typography>
+              <i>Price: </i>
+              {price}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary">
+              View
+            </Button>
+              <Button size="small" color="primary" onClick={this.handleOpen}>
+                Edit
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+          </>
+      );
+}
 };
 
-export default ProductListItem;
