f6201015be4649115cee10211288783ba4ef17fa
CodingRuslan <orion.hola@gmail.com>
2019-12-13 01:59:32 +0300

    feat: add edit form

--- src/components/edit-form/edit-form.js
+++ src/components/edit-form/edit-form.js
@@ -1,27 +1,30 @@
-import React, {Component} from 'react';
-import Modal from '@material-ui/core/Modal';
-import './edit-form.css'
+import React, { Component } from "react";
+import Modal from "@material-ui/core/Modal";
+import "./edit-form.css";
 
-export default class EditForm extends Component{
-    constr
+export default class EditForm extends Component {
+  state = {
+    open: false
+  };
 
-    render() {
-        return (
-            <div>
-                <Modal
-                    aria-labelledby="simple-modal-title"
-                    aria-describedby="simple-modal-description"
-                    open={open}
-                    onClose={handleClose}
-                >
-                    <div className="paper">
-                        <h2 id="simple-modal-title">Text in a modal</h2>
-                        <p id="simple-modal-description">
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </p>
-                    </div>
-                </Modal>
-            </div>
-        );
-}
+  render() {
+    const { open, handleClose } = this.props;
+    return (
+      <div>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={handleClose}
+        >
+          <div className="paper">
+            <h2 id="simple-modal-title">Text in a modal</h2>
+            <p id="simple-modal-description">
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </p>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 }
--- src/components/product-list-item/product-list-item.js
+++ src/components/product-list-item/product-list-item.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -9,9 +9,7 @@ import Typography from "@material-ui/core/Typography";
 import EditForm from "../edit-form";
 import "./product-list-item.css";
 
-
-export default class ProductListItem extends Component{
-
+export default class ProductListItem extends Component {
   constructor(props) {
     super(props);
 
@@ -20,11 +18,17 @@ export default class ProductListItem extends Component{
     };
   }
 
-  handleOpen = () => {
+  handleEditOpen = () => {
     this.setState({
       ...this.state,
       openEditWindow: true
-    })
+    });
+  };
+
+  handleEditClose = () => {
+    this.setState({
+      openEditWindow: false
+    });
   };
 
   render() {
@@ -32,40 +36,46 @@ export default class ProductListItem extends Component{
     const { title, description, price } = product; // productId, libId,title,description, price,stars,parent, tag1,tag2, tag3
 
     return (
-        <>
-      <EditForm/>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card className="card">
-          <CardMedia
-            className="cardMedia"
-            image="https://source.unsplash.com/random"
-            title="Image title"
-          />
-          <CardContent className="cardContent">
-            <Typography gutterBottom variant="h5" component="h2">
-              {title}
-            </Typography>
-            <Typography>
-              <i>Description: </i>
-              {description}
-            </Typography>
-            <Typography>
-              <i>Price: </i>
-              {price}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small" color="primary">
-              View
-            </Button>
-              <Button size="small" color="primary" onClick={this.handleOpen}>
+      <>
+        <EditForm
+          open={this.state.openEditWindow}
+          handleClose={this.handleEditClose}
+        />
+        <Grid item xs={12} sm={6} md={4}>
+          <Card className="card">
+            <CardMedia
+              className="cardMedia"
+              image="https://source.unsplash.com/random"
+              title="Image title"
+            />
+            <CardContent className="cardContent">
+              <Typography gutterBottom variant="h5" component="h2">
+                {title}
+              </Typography>
+              <Typography>
+                <i>Description: </i>
+                {description}
+              </Typography>
+              <Typography>
+                <i>Price: </i>
+                {price}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" color="primary">
+                View
+              </Button>
+              <Button
+                size="small"
+                color="primary"
+                onClick={this.handleEditOpen}
+              >
                 Edit
               </Button>
             </CardActions>
           </Card>
         </Grid>
-          </>
-      );
+      </>
+    );
+  }
 }
-};
-
